a10e89c0f2c8c55a8ed437e4fd63935d1e9c2988
Krzosa <krzosa.karol@gmail.com>
2019-10-16 15:58:39 +0200

    done

---
+++ lab1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[]){
+        ICalc calc = new Calc();
+        double a = 5; double b = 6;
+        System.out.println(calc.add(a,b));
+    }
+
+}
