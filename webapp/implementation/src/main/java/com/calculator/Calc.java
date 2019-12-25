25165a92fc29cdbbf82835717966ddd484919265
Krzosa <krzosa.karol@gmail.com>
2019-12-25 20:37:23 +0100

    Added: working calculator yay. Most work done.

--- webapp/implementation/src/main/java/com/calculator/Calc.java
+++ webapp/implementation/src/main/java/com/calculator/Calc.java
@@ -4,8 +4,11 @@ import java.io.File;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.Character.isDigit;
 import static java.lang.System.exit;
 
 public class Calc implements ICalc, Cloneable {
@@ -81,5 +84,82 @@ public class Calc implements ICalc, Cloneable {
             System.out.println(e);
             return -1;
         }
+    }
+    //we are taking a math problem in string. We divide it into sections with "+" "-" as separators
+    // (operators saved are into a buffer)
+    //then we combine them from digits into numbers
+    //lastly we use the operators that were saved into the buffer and output the result
+
+    @Override
+    public int calculate(String problem) {
+        int problemLength = problem.length();
+        List<Character> operators = new ArrayList<>();
+        List<List<Integer>> numbersMason = new ArrayList<>();
+        numbersMason.add(new ArrayList<>());
+        int ii = 0;
+        //dividing numbers into separate arrays with "+" etc. as separators
+        for(int i = 0; i<problemLength;i++){
+
+            if(isDigit(problem.charAt(i))){
+                numbersMason.get(ii).add(Character.getNumericValue(problem.charAt(i)));
+            }
+            else if(problem.charAt(i)=='+'){
+                operators.add('+');
+                numbersMason.add(new ArrayList<>());
+                ii++;
+            }
+            else if(problem.charAt(i)=='-'){
+                operators.add('-');
+                numbersMason.add(new ArrayList<>());
+                ii++;
+            }
+            else if(problem.charAt(i)=='*'){
+                operators.add('*');
+                numbersMason.add(new ArrayList<>());
+                ii++;
+            }
+            else if(problem.charAt(i)=='/'){
+                operators.add('/');
+                numbersMason.add(new ArrayList<>());
+                ii++;
+            }
+        }
+        int result = 0;
+        int power = 0;
+        //converting array of digits into their number counterparts
+        for(int i = numbersMason.size()-1;i>=0;i--){
+            power = 0;
+            for(int j = numbersMason.get(i).size()-1;j>=0;j--){
+                numbersMason.get(i).set(j, (int) (numbersMason.get(i).get(j)*Math.pow(10,power)));
+                power++;
+            }
+        }
+        //adding the numbers that we got
+        for(int i = numbersMason.size()-1;i>=0;i--){
+            power = 0;
+            for(int j = numbersMason.get(i).size()-1;j>0;j--){
+                numbersMason.get(i).set(0, (int) (numbersMason.get(i).get(0)+numbersMason.get(i).get(j)));
+                power++;
+            }
+        }
+        //here we are operating on the numbers with operators that were saved in a buffer
+        result = numbersMason.get(0).get(0);
+        for(int i = 0; i<numbersMason.size()-1;i++){
+            switch(operators.get(i)){
+                case '+':
+                    result+=numbersMason.get(i+1).get(0);
+                    break;
+                case '-':
+                    result-=numbersMason.get(i+1).get(0);
+                    break;
+                case '*':
+                    result*=numbersMason.get(i+1).get(0);
+                    break;
+                case '/':
+                    result/=numbersMason.get(i+1).get(0);
+                    break;
+            }
+        }
+        return result;
     }
 }
--- webapp/webapp/src/main/java/com/calculator/calculator/Calculator.java
+++ webapp/webapp/src/main/java/com/calculator/calculator/Calculator.java
@@ -1,17 +1,27 @@
 package com.calculator.calculator;
 
+import com.calculator.Calc;
+import com.calculator.ICalc;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Calculator {
+
+    ICalc calcObj = new Calc();
+
     @GetMapping("/calculator")
     public String calculatorPage(){
         return "calculator";
     }
     @PostMapping("/calculator")
-    public String calcApp(){
+    public String calcApp(@RequestParam(name="problem", required=true) String problem, Model model){
+        int result = calcObj.calculate(problem);
+        model.addAttribute("resultText", "Result of your problem: ");
+        model.addAttribute("result",result);
         return "calculator";
     }
 
