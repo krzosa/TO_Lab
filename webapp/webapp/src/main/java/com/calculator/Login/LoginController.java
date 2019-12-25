dcefcd8dce13abbc00742dc81cee3e37a245f32d
Krzosa <krzosa.karol@gmail.com>
2019-12-25 16:04:33 +0100

    Fixed: maven spring boot startup error

--- webapp/webapp/src/main/java/com/calculator/Login/LoginController.java
+++ webapp/webapp/src/main/java/com/calculator/Login/LoginController.java
@@ -15,12 +15,12 @@ public class LoginController {
     @Autowired
     UserValidation uv;
 
-    @GetMapping(value= "/login")
+    @GetMapping(value= "/index")
     public String loginPage(){
         return "index";
     }
 
-    @PostMapping(value = "/login")
+    @PostMapping(value = "/index")
     public String login(@RequestParam(name="login", required = true) String login,
                         @RequestParam(name="pass", required = true) String pass,
                         Model model){
--- webapp/webapp/src/main/java/com/calculator/calculator/Calculator.java
+++ webapp/webapp/src/main/java/com/calculator/calculator/Calculator.java
@@ -1,12 +1,17 @@
 package com.calculator.calculator;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class Calculator {
-    @RequestMapping("/calculator")
+    @GetMapping("/calculator")
     public String calculatorPage(){
+        return "calculator";
+    }
+    @PostMapping("/calculator")
+    public String calcApp(){
         return "calculator";
     }
 
